1eb1f5868b7bea203000408678e86b35a5af2028
masaya sato <masataro1201@gmail.com>
2026-03-23 17:26:32 +0900

    一括代入の追記

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -23,12 +23,11 @@ class TodoController extends Controller
 
     public function store(Request $request)
     {
-        $content = $request->input('content');
-        // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
+        $inputs = $request->all();
+        dd($inputs);
+
         $todo = new Todo();
-        // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-        $todo->content = $content;
-        // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+        $todo->fill($inputs);
         $todo->save();
 
         return redirect()->route('todo.index');
