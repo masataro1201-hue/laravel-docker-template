c0d235945eeb386da5477449642ae88a53e7461b
masaya sato <masataro1201@gmail.com>
2026-03-23 17:16:30 +0900

    DBに保存する機能追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Todo;
 
+
 class TodoController extends Controller
 {
     public function index()
@@ -18,5 +19,18 @@ class TodoController extends Controller
     public function create()
     {
         return view('todo.create');
+    }
+
+    public function store(Request $request)
+    {
+        $content = $request->input('content');
+        // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
+        $todo = new Todo();
+        // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
+        $todo->content = $content;
+        // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+        $todo->save();
+
+        return redirect()->route('todo.index');
     }
 }
