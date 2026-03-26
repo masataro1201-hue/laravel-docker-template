2111a054318708fde2f83cb116ed8189252a87c5
masaya sato <masataro1201@gmail.com>
2026-03-26 11:42:01 +0900

    review2.mdの修正

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -60,6 +60,7 @@ class TodoController extends Controller
     public function delete($id)
     {
         $todo = $this->todo->find($id);
+        dd($todo);
         $todo->delete();
 
         return redirect()->route('todo.index');
